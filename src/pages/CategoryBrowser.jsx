29a839dc60e13feea482522bf55d3f695aeb8419
Myo Min Thu <myominthu819@gmail.com>
2026-05-25 22:41:47 +0630

    updated

--- src/pages/CategoryBrowser.jsx
+++ src/pages/CategoryBrowser.jsx
@@ -161,7 +161,7 @@ const CategoryBrowser = () => {
               {/* Search Bar */}
               <div className="max-w-md mx-auto">
                 <div className="relative">
-                  <MagnifyingGlassIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-slate-600" />
+                  <MagnifyingGlassIcon className="absolute left-4 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400 dark:text-white/60" />
                   <input
                     type="text"
                     value={searchQuery}
